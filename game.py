011d9278e159f489058984f0db76779175c24748
Rasul <rasul.rzaev.2014@mail.ru>
2026-06-01 22:21:36 +0800

    feat: добавил подсказки горячо/холодно

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
+# game.py (версия Пети)
 import random
+
 print("🎮 Добро пожаловать в игру 'Угадай число'!")
 print("Я загадал число от 1 до 10")
 
@@ -6,7 +8,16 @@ secret = random.randint(1, 10)
 
 while True:
     guess = int(input("Твоё предположение: "))
-    #Файл для Димы!!!
+    
+    # ПЕТЯ ДОБАВИЛ ПОДСКАЗКИ:
+    diff = abs(guess - secret)
+    if diff <= 2:
+        print("🔥 ГОРЯЧО!")
+    elif diff <= 4:
+        print("😐 ТЕПЛО...")
+    else:
+        print("❄️ ХОЛОДНО!")
+    
     if guess < secret:
         print("❌ Мало!")
     elif guess > secret:
