70a61ac6d379d1b72b5613cee4865e9393fa7acd
Rasul <rasul.rzaev.2014@mail.ru>
2026-06-01 22:35:17 +0800

    feat: добавила уровни сложности 1-10/1-50/1-100

--- game.py
+++ game.py
@@ -1,12 +1,29 @@
+# game.py (версия Маши)
 import random
-print("🎮 Добро пожаловать в игру 'Угадай число'!")
-print("Я загадал число от 1 до 10")
 
-secret = random.randint(1, 10)
+print("🎮 Добро пожаловать в игру 'Угадай число'!")
+
+# МАША ДОБАВИЛА ВЫБОР СЛОЖНОСТИ:
+print("Выбери сложность:")
+print("1 - Легко (1-10)")
+print("2 - Средне (1-50)")
+print("3 - Сложно (1-100)")
+
+choice = input("Твой выбор (1-3): ")
+if choice == "1":
+    max_num = 10
+elif choice == "2":
+    max_num = 50
+else:
+    max_num = 100
+
+print(f"Я загадал число от 1 до {max_num}")
+
+secret = random.randint(1, max_num)
 
 while True:
     guess = int(input("Твоё предположение: "))
-    #Файл для Димы!!!
+    
     if guess < secret:
         print("❌ Мало!")
     elif guess > secret:
