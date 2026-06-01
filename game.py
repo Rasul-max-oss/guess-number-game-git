07b7ad884d5f6a4dfc7c1204a6ccda981cb71b6c
Rasul <rasul.rzaev.2014@mail.ru>
2026-06-01 22:09:18 +0800

    feat: базовая версия игры

---
+++ game.py
@@ -0,0 +1,16 @@
+import random
+print("🎮 Добро пожаловать в игру 'Угадай число'!")
+print("Я загадал число от 1 до 10")
+
+secret = random.randint(1, 10)
+
+while True:
+    guess = int(input("Твоё предположение: "))
+    
+    if guess < secret:
+        print("❌ Мало!")
+    elif guess > secret:
+        print("❌ Много!")
+    else:
+        print(f"✅ Угадал! Это было число {secret}")
+        break
